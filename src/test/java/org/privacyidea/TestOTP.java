6e67ef4f8019b5936b0e3b69e6ad424970d4737b
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-03-29 15:40:29 +0200

    Create TestGetTokenInfo

--- src/test/java/org/privacyidea/TestOTP.java
+++ src/test/java/org/privacyidea/TestOTP.java
@@ -25,7 +25,6 @@ import org.mockserver.model.HttpResponse;
 import org.mockserver.model.MediaType;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
