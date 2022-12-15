a3f1eec4592ce8030eb7986c5f2eabf136bf9ec2
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-12-15 14:20:37 +0100

    Update challenge and consts with image

--- src/main/java/org/privacyidea/PIConstants.java
+++ src/main/java/org/privacyidea/PIConstants.java
@@ -64,6 +64,7 @@ public class PIConstants
     public static final String TOKEN = "token";
     public static final String PREFERRED_CLIENT_MODE = "preferred_client_mode";
     public static final String MESSAGE = "message";
+    public static final String IMAGE = "image";
     public static final String MESSAGES = "messages";
     public static final String MULTI_CHALLENGE = "multi_challenge";
     public static final String ATTRIBUTES = "attributes";
