b197d050a98d9217827339cdd8b4977fdafbb52c
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-01-13 15:32:04 +0100

    formatting

--- src/main/java/org/privacyidea/Challenge.java
+++ src/main/java/org/privacyidea/Challenge.java
@@ -22,47 +22,33 @@ public class Challenge
 {
     private final List<String> attributes = new ArrayList<>();
     private final String serial;
-    private final String client_mode;
+    private final String clientMode;
     private final String message;
-    private final String transaction_id;
+    private final String transactionId;
     private final String type;
     private final String image;
 
-    public Challenge(String serial, String message, String client_mode, String image, String transaction_id, String type)
+    public Challenge(String serial, String message, String clientMode, String image, String transactionId, String type)
     {
         this.serial = serial;
         this.message = message;
-        this.client_mode = client_mode;
+        this.clientMode = clientMode;
         this.image = image;
-        this.transaction_id = transaction_id;
+        this.transactionId = transactionId;
         this.type = type;
     }
 
-    public List<String> getAttributes()
-    {
-        return attributes;
-    }
+    public List<String> getAttributes() { return attributes; }
 
-    public String getSerial()
-    {
-        return serial;
-    }
+    public String getSerial() { return serial; }
 
-    public String getMessage()
-    {
-        return message;
-    }
-    public String getClientMode() { return client_mode; }
+    public String getMessage() { return message; }
+
+    public String getClientMode() { return clientMode; }
 
     public String getImage() { return image.replaceAll("\"", ""); }
 
-    public String getTransactionID()
-    {
-        return transaction_id;
-    }
+    public String getTransactionID() { return transactionId; }
 
-    public String getType()
-    {
-        return type;
-    }
+    public String getType() { return type; }
 }
